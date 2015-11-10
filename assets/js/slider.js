953e3cb8b17bfc5a15c958876d1d119dd70cf568
Hunain Mehmood <hunain60@gmail.com>
2015-11-10 17:25:36 -0500

    dude changes

---
+++ assets/js/slider.js
@@ -0,0 +1,20 @@
+(function($) {
+	
+	$(document).ready(){
+		images[]={'images/slider/dude0.png',
+		'images/slider/dude1.png',
+		'images/slider/dude2.png',
+		'images/slider/dude3.png',
+		'images/slider/dude4.png',
+		'images/slider/dude5.png',
+		'images/slider/dude6.png',
+		'images/slider/dude7.png',
+		'images/slider/dude8.png',
+		'images/slider/dude9.png',
+		'images/slider/dude10.png',
+		'images/slider/dude11.png'}
+		
+		
+	}
+	
+}
